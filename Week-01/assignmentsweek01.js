aabcc74ba8b0163f86f6e9228dc87d503b13a19d
deepak03-git <deepakraj133926@gmail.com>
2026-04-01 23:32:02 +0530

    Add files via upload
    Week 01 assignments

---
+++ Week-01/assignmentsweek01.js
@@ -0,0 +1,96 @@
+// // // Assignment 1 (odd or even)
+// // let int=5;
+// // if (int%2==0)
+// // {
+// // console.log ("the given number " + int + " is even")
+// // } else {
+// //     console.log ("the given number " + int + " is odd")
+// // }
+
+
+// //Assignment 2 () Reverse the string and palindrome
+
+// //  let str="level"; 
+// //  let reverse="";
+// //  for (let i=str.length;i>=0;i--){
+// //      reverse = reverse + str.charAt(i);
+// //      //console.log ("the string got reversed " + reverse);
+// //  }
+// // console.log ("the string got reversed = " + reverse);
+// //  if (str==reverse){
+// //     console.log("Palindrome is correct"); 
+// //     } else
+// //          {
+// //         console.log ("palindrome is incorrect")
+// //     }
+ 
+
+// // Assignment 3 categorize a number as positive, negative, or zero using conditional statements in JavaScript
+
+// let num = 0;
+// switch (true) {
+//     case (num >0):
+//      console.log (" the given number is positive");
+//         break;
+//     case ( num < 0):
+//         console.log ("the given number is negative");
+//      break;
+//      case ( num == 0):
+//         console.log ("the given number is zero ");
+//         break;
+// }
+
+
+// Assignment 4 if-else and switch case
+// // if else
+//  let  browserName= "chrome";
+//  if (browserName=="chrome") {
+// console.log ("launch the chrome")
+//  } else {
+//     console.log ("don't launch")
+//  }
+
+// //switch
+// let testType ="sanity";
+// switch (testType) {
+//     case ("smoke"):
+//         console.log ("the execution is smoke")
+//         break;
+//     case ("sanity"):
+//         console.log("the execution is sanity")
+//         break;
+//     // default:
+//     //     console.log ("the execution is smoke")
+//     //     break;
+// }
+
+
+// Assignment 5 looping statements
+
+// for (i=0; i<=25; i++){
+//     if (i%2!=0) {
+//         console.log ("Below are the listed odd numbers : " + i)
+//     }
+// //console.log("Below are the listed odd numbers")
+// }
+
+
+// Assignment 6 print the student grade calculation
+
+// function student(score) {
+// switch (true) {
+//     case (score>80):
+//         console.log ("The grade is A")
+//         break;
+//     case (score>60):
+//         console.log ("The grade is B")
+//         break;
+// }
+// }
+// student(59)
+
+for (i=10;i<=1;i--) 
+    {
+    console.log(i);
+}
+
